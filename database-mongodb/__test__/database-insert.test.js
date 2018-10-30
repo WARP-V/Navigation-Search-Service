966e14121461393e16c3799761ee4ef6d489a987
Rao Guttula <rao@Raos-MacBook-Pro.local>
2018-10-29 19:39:32 -0700

    it replaced with test function

--- database-mongodb/__test__/database-insert.test.js
+++ database-mongodb/__test__/database-insert.test.js
@@ -6,7 +6,7 @@ test('jest is working', () => {
 });
 
 describe('methods for the Shoe Group components', () => {
-  it('insdert method - should insert a document(row) into collection (table)', async () => {
+  test('insdert method - should insert a document(row) into collection (table)', async () => {
     const mockShoe = {shoeID: '555555-555', name: 'mockShoe', imageUrls: 'https://s3.amazonaws.com/warp-v/images/136027_148.jpeg', productGroup: 'Air Jordan Mid', searchStr: 'Air Jordan', price: '$555'};
     ShoeGroup.ShoeGroup.create(mockShoe);
     // const insertedShoe = ShoeGroup.findOne({shoeID: '555555-555'});
